769ff5da7aec7cf8ea747142372334add962f4cb
schmidln <schmidln@bc.edu>
2025-01-20 18:14:32 -0500

    Initial Commit

---
+++ Football/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Football
+//
+//  Created by Lucas Schmidt on 1/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("What is Football to You?")
+                .font(.largeTitle)
+                .fontWeight(.thin)
+                .foregroundStyle(.green)
+            
+            HStack {
+                Image(systemName: "figure.american.football")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.blue)
+                Image(systemName: "figure.australian.football")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.indigo)
+                Image(systemName: "figure.soccer")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.purple)
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Lucas Schmidt on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
